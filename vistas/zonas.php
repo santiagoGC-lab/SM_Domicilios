04f40162ca513d14bb19e241a0b35afb0093f318
santiagi <santiagogarcia0434@gmail.com>
2025-07-04 10:41:37 -0500

    medio arreglo

--- vistas/zonas.php
+++ vistas/zonas.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>SM - Zonas de Entrega</title>
-    <link rel="stylesheet" href="../componentes/base.css" />
+    <link rel="stylesheet" href="../componentes/dashboard.css" />
     <link rel="stylesheet" href="../componentes/zonas.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
 </head>
