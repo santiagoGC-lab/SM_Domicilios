421bff0d78526b440efa345408ab5155b393fea5
santiagi <santiagogarcia0434@gmail.com>
2025-07-07 07:07:19 -0500

    quitar comentarios

--- servicios/cerrar_sesion.php
+++ servicios/cerrar_sesion.php
@@ -1,21 +1,16 @@
 <?php
-// Start session if not already started
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-// Destroy all session data
 session_destroy();
 
-// Clear session cookies
 if (isset($_COOKIE[session_name()])) {
     setcookie(session_name(), '', time()-3600, '/');
 }
 
-// Clear all session variables
 $_SESSION = array();
 
-// Redirect to login page
 header("Location: ../login.php");
 exit();
 ?>
